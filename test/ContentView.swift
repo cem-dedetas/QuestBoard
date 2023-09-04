6f4696c55e520c7eb349d4f26d837721768dbe5f
cemdedetas <cemdedetas@gmail.com>
2023-09-04 14:47:35 +0300

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Cem Dedetas on 4.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Cem Dedetas on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
